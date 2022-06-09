c21c3071464191fea42e8fdd541e291ac56bda62
nick-casa <nc16@princeton.edu>
2022-06-09 11:51:37 -0400

    added fcl

--- packages/example-next/pages/index.tsx
+++ packages/example-next/pages/index.tsx
@@ -4,11 +4,44 @@ import MetaMaskCard from '../components/connectorCards/MetaMaskCard'
 import NetworkCard from '../components/connectorCards/NetworkCard'
 import WalletConnectCard from '../components/connectorCards/WalletConnectCard'
 import ProviderExample from '../components/ProviderExample'
-import Bleumi from '../components/connectorCards/Bleumi'
+
+//import Bleumi from '../components/connectorCards/Bleumi'
+
+import Head from "next/head"
+import "../flow/config";
+import { useState, useEffect } from "react";
+import * as fcl from "@onflow/fcl";
+
 
 export default function Home() {
+  // Tracks users status 
+  const [user, setUser] = useState({loggedIn: null, addr: null})
+
+  // Set the user to the current user 
+  useEffect(() => fcl.currentUser.subscribe(setUser),[])
+
+  const AuthedState = () => {
+    return (<div>
+      <div>Address {user?.addr ?? "No Address"}</div>
+      <button onClick={fcl.unauthenticate}>Log Out</button>
+    </div>)
+  }
+  const UnauthenticatedState = () => {
+    return (
+      <div>
+        <button onClick={fcl.logIn}>Log In</button>
+        <button onClick={fcl.signUp}>Sign Up</button>
+      </div>)
+  }
+
   return (
     <>
+      <Head>
+        <title>Test App</title>
+        <meta name="description" content="Test"/>
+        <link rel="icon" href="/favicon.png"/>
+      </Head>
+
       <ProviderExample />
       <div style={{ display: 'flex', flexFlow: 'wrap', fontFamily: 'sans-serif' }}>
         <MetaMaskCard />
@@ -16,8 +49,9 @@ export default function Home() {
         <CoinbaseWalletCard />
         <NetworkCard />
         <GnosisSafeCard />
-        <Bleumi/>
+        {user.loggedIn ? <AuthedState /> : <UnauthenticatedState/>}
       </div>
     </>
   )
+
 }
